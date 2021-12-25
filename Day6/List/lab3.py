2d3c624256facd66c249e6bacf2f959d747e9e30
Sahar3ha <saharsha.xtha.3@gmail.com>
2021-12-25 21:27:39 +0545

    hom

--- Day6/List/lab3.py
+++ Day6/List/lab3.py
@@ -1,4 +1,3 @@
 #Write a Python program to get the largest number from a list.
-#Write a Python program to get the largest number from a list.
 list=[11,35,66,7789,9]
 print(f"The largest number is : ", max(list))
--- Day6/List/lab5.py
+++ Day6/List/lab5.py
@@ -1,3 +1,8 @@
 #Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
-
-input=input("Enter something : ")
+list1 = ['aba','121','kgf','abc']
+y=0
+for i in list1:
+    if len(i)>2:
+        if i[0]==i[len(i)-1]:
+            y=y+1
+print(y)            
